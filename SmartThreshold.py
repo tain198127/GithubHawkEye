5121c67fda6bc7f76909b1812a262f0d30b7b99e
DaneBrown <tain198127@163.com>
2019-04-25 17:14:48 +0800

    1. 限流器：增加了统计功能

--- SmartThreshold.py
+++ SmartThreshold.py
@@ -1,17 +1,21 @@
 # coding: utf-8
 # 限流器
+import Queue
+import datetime
 import logging
 import threading
 import time
 
 import schedule
 
-# 5000开始的时间
-FirstCallTime = time.time()
-EndCall = None
+# 最开始的时间
+BeginCallTime = datetime.datetime.now()
+# 最后一次时间
+EndCallTime = None
 InvokeTimes = 0
 Threshold = [(1000, 0.1), (2000, 0.2), (3000, 0.4), (4000, 0.8), (5000, 1.8)]
-
+AllInvokeCount = 0
+q = Queue.LifoQueue(maxsize=400)
 logger = logging.getLogger()
 
 
@@ -20,8 +24,9 @@ class SmartThreshold:
     @staticmethod
     def count_keep_rate(fn):
         def run(*args, **kwargs):
-            global Threshold, InvokeTimes, EndCall
+            global Threshold, InvokeTimes, EndCallTime, AllInvokeCount, BeginCallTime
             sleeptime = 0
+            start_ts = datetime.datetime.now()
             for t in Threshold:
                 if InvokeTimes >= t[0]:
                     sleeptime = t[1]
@@ -30,10 +35,19 @@ class SmartThreshold:
             logger.info("调用次数{}- 休眠时间{}".format(InvokeTimes, sleeptime))
             if sleeptime > 0:
                 time.sleep(sleeptime)
-            start_ts = time.time()
-            end_ts = time.time()
-            EndCall = time.time()
+
+            end_ts = datetime.datetime.now()
+            EndCallTime = datetime.datetime.now()
             InvokeTimes = InvokeTimes + 1
+            AllInvokeCount = AllInvokeCount + 1
+
+            span = (EndCallTime - BeginCallTime).seconds
+            if span > 10:
+                if q.full():
+                    q.queue.pop()
+                q.put((BeginCallTime, EndCallTime, AllInvokeCount))
+                AllInvokeCount = 0
+                BeginCallTime = datetime.datetime.now()
             result = fn(*args, **kwargs)
             return result
 
@@ -51,8 +65,15 @@ class SmartThreshold:
         job_thread = threading.Thread(target=job_func)
         job_thread.start()
 
+    @staticmethod
+    def show_freq():
+        if not q.empty():
+            for f in q.queue[:10]:
+                print("开始时间:{},结束时间:{},调用次数:{}".format(f[0], f[1], f[2]))
+
 
 schedule.every(1).seconds.do(SmartThreshold.threadInovker, SmartThreshold.dcreaseThreshold)
+schedule.every(10).seconds.do(SmartThreshold.threadInovker, SmartThreshold.show_freq)
 
 
 # 守护进程
