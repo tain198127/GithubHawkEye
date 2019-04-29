967d2862a8cb80873865e1995b0bb96ebdc06276
DaneBrown <tain198127@163.com>
2019-04-29 13:20:15 +0800

    1. 打印rate limit

--- main.py
+++ main.py
@@ -53,6 +53,10 @@ Contributor_Org_Rel.create_table()
 LastQueryConfig.create_table()
 
 lastQueryConfig = None
+print(auth)
+
+
+# github = Github(auth)
 
 
 def printRepository(repository):
@@ -69,22 +73,6 @@ def printRepository(repository):
     print("created at:", repository.created_at)
     print("description", repository.description)
     print("=========================================================")
-
-
-# 获取前3万赞的java项目的数量分布
-def getRepoStartRangeFreq(language):
-    g = Github(auth)
-    repositories = g.search_repositories("stars:>=800000 language:{}".format(language), "stars", "desc")
-    for repo in repositories:
-        logger.info("stargazers_count is {}".format(repo.stargazers_count))
-    for i in reversed(range(1, 101)):
-        filterStr = "stars:{}..{} language:{}".format((i - 1) * 1000 + 1, i * 1000, language)
-        time.sleep(2)
-        repositories = g.search_repositories(filterStr, "stars", "desc")
-        logger.info("{},{},{}".format((i - 1) * 1000 + 1, i * 1000, repositories.totalCount))
-    time.sleep(2)
-    lessrepo = g.search_repositories("stars:1..1000 language:{}".format(language), "stars", "desc")
-    logger.info("{},{},{}".format(1, 1000, lessrepo.totalCount))
 
 
 @SmartThreshold.count_keep_rate
@@ -104,6 +92,14 @@ def processContributorOrgs(contributor):
 
 @SmartThreshold.count_keep_rate
 def processRepo(repo):
+    github = Github(auth)
+
+    rl = github.get_rate_limit()
+    print(rl.rate)
+    print("rate_limiting:")
+    print(github.rate_limiting)
+    print("rate_limiting_resettime:")
+    print(github.rate_limiting_resettime)
     mysql_db.connection()
     # with mysql_db.atomic():
     # 插入repo
@@ -159,10 +155,11 @@ def processRepo(repo):
 # max: step*max = max range,must > 1
 # displayAbove:is display the info more than max
 def getRepoRangeFreq(language, condition, steps, min, max, displayAbove):
-    g = Github(auth)
+    github = Github(auth)
+
     if (displayAbove):
-        repositories = g.search_repositories("{}:>={} language:{}".format(condition, (max + 1) * steps, language),
-                                             condition, "desc")
+        repositories = github.search_repositories("{}:>={} language:{}".format(condition, (max + 1) * steps, language),
+                                                  condition, "desc")
         for repo in repositories:
             logger.info("{}_count is {}".format(condition, repo.stargazers_count))
     for i in range(min, max + 1):
@@ -170,12 +167,9 @@ def getRepoRangeFreq(language, condition, steps, min, max, displayAbove):
         sidx = i
         logger.info(filterStr)
         LastQueryConfig().add_config(LastQueryConfig(startIdx=sidx, endIdx=max, steps=steps))
-        repositories = g.search_repositories(filterStr, condition, "desc")
+        repositories = github.search_repositories(filterStr, condition, "desc")
         for r in repositories:
             processRepo(r)
-        # logger.info("{},{},{}".format((i - 1) * steps + 1, i * steps, repositories.totalCount))
-    # lessrepo = g.search_repositories("{}:1..{} language:{}".format(condition, steps, language), condition, "desc")
-    # logger.info("{},{},{}".format(1, steps, lessrepo.totalCount))
 
 
 # 根据fork获取项目分布
@@ -192,22 +186,5 @@ def getRepoForkRangeFreq(language):
     getRepoRangeFreq(language, "stars", steps, min, max, False)
 
 
-# 获取1000以内的赞的分布
-def getRepoStartLess1000RangeFreq(language):
-    g = Github("36827d66490ddbbd09ba07b25fb32144132709e2")
-    for i in reversed(range(1, 101)):
-        filterStr = "stars:{}..{} language:{}".format((i - 1) * 10 + 1, i * 10, language)
-        time.sleep(2)
-        repositories = g.search_repositories(filterStr, "stars", "desc")
-        logger.info("{},{},{}".format((i - 1) * 10 + 1, i * 10, repositories.totalCount))
-    time.sleep(2)
-    lessrepo = g.search_repositories("stars:1..10 language:{}".format(language), "stars", "desc")
-    logger.info("{},{},{}".format(1, 1000, lessrepo.totalCount))
-
-
-# getRepoStartRangeFreq("java")
-
-
-# getRepoStartLess1000RangeFreq("JavaScript")
 if __name__ == '__main__':
     getRepoForkRangeFreq("Java")
